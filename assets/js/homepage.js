00e30ea615c2d53bac8f31b85ee565d8cbdd1739
JideOgun <JideOgun@github.com>
2021-12-07 10:25:45 -0600

    added feature to filter results by language using a different github api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term")
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -91,4 +92,34 @@ var getUserRepos = function(user) {
       }
   }
    
+var getFeaturedRepos = function (language) {
+var apiUrl = "https://api.github.com/search/repositories?q=" + language + "is:featured&sort=help-wanted-issues";
+fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+        response.json().then(function(data){
+            displayRepos(data.items, language);
+        });
+        
+    }
+    else {
+        alert('Error: Github user not found');
+    }
+}) ;
+}
+
+var buttonClickHandler = function(event) {
+
+var language = event.target.getAttribute("data-language");
+console.log(language);
+
+if(language) {
+    getFeaturedRepos(language);
+
+    // clear old content
+    repoContainerEl.textContent="";
+}
+
+}
+
   userFormEl.addEventListener("submit", formSubmitHandler);
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
